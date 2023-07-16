538b6c107b34911426ff69562dacbade134c1a37
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-07-16 15:06:50 +0300

    Query annotation and Native Queries

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -3,7 +3,7 @@ package repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 
 import com.roaa.internship.spring.data.jpa.entity.Student;
 
@@ -16,4 +16,24 @@ public interface StudentRepository extends JpaRepository<Student, Long>{
 	 List<Student> findByLastNameNotNull();
 	 
 	 List<Student> findByGuardianName(String guardianName);
+	 
+	 Student findByFirstNameAndLastName(String firstName, String lastName);
+	 
+	 //JPQL
+	 @Query("select s from Student s where s.emailId = ?1")
+	 Student getStudentByEmailAddress(String EmailId);
+	 
+	 
+	//JPQL
+	@Query("select s.firstNmae from Student s where s.emailId = ?1")
+	String getStudentFirstNameByEmailAddress(String EmailId);
+	
+	
+	@Query(
+			value="SELECT* FROM tbl_student s where s.email_adddress = ?1",
+			nativeQuery=true
+	)
+	Student getStudentByEmailAddressNative(String emailid);
+	
+	
 }
