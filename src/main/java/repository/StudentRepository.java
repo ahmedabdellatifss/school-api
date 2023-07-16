4b9d078d24bdc6380b0768b04d45d355a65202c3
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-07-16 18:04:18 +0300

    Transaction and Modifying

--- src/main/java/repository/StudentRepository.java
+++ src/main/java/repository/StudentRepository.java
@@ -3,7 +3,10 @@ package repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.roaa.internship.spring.data.jpa.entity.Student;
 
@@ -26,14 +29,34 @@ public interface StudentRepository extends JpaRepository<Student, Long>{
 	 
 	//JPQL
 	@Query("select s.firstNmae from Student s where s.emailId = ?1")
-	String getStudentFirstNameByEmailAddress(String EmailId);
+	String getStudentFirstNameByEmailAddress(String emailId);
 	
 	
+	// Native Query 
 	@Query(
 			value="SELECT* FROM tbl_student s where s.email_adddress = ?1",
 			nativeQuery=true
 	)
-	Student getStudentByEmailAddressNative(String emailid);
+	Student getStudentByEmailAddressNative(String emailId);
+	
+	
+	// Native Named Param
+	@Query(
+		value="SELECT* FROM tbl_student s where s.email_adddress = :emailId",
+		nativeQuery=true
+	)
+	Student getStudentByEmailAddressNativeNamedParam(@Param("emailId") String emailId);
+	
+	
+	@Modifying
+	@Transactional
+	@Query(
+	
+			value="update tbl_student set first-name = ?1 where email_address = ?2",
+			nativeQuery = true
+	)
+	int updateStudentNameByEmailId(String firstName, String emailId);
+	
 	
 	
 }
